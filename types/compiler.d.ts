a2ff8433ef4762436a43915e2c9fb38e922ff29f
CainHappyfish <280971620@qq.com>
2024-09-02 21:03:22 +0800

    ✨ feat(custom): 编译器基本功能

--- types/compiler.d.ts
+++ types/compiler.d.ts
@@ -1,14 +1,57 @@
-/* eslint-disable @typescript-eslint/no-unsafe-function-type */
-import { cainVm } from './mvvm'
-
-export interface VmCompiler {
-  /**
-   * 要解析的HTML元素
-   * */
-  $el: HTMLElement
+/**
+ * 状态机状态
+ * */
+export const State = {
 
   /**
-   * 解析后的vm
+   * 初始状态
    * */
-  $vm: cainVm
+  initial: 1,
+
+  /**
+   * 标签开始状态
+   * */
+  tagStart: 2,
+
+  /**
+   * 标签名称状态
+   * */
+  tagName: 3,
+
+  /**
+   * 文本状态
+   * */
+  text: 4,
+
+  /**
+   * 结束标签状态
+   * */
+  tagEnd: 5,
+
+  /**
+   * 结束标签名称状态
+   * */
+  tagEndName: 6
+}
+
+/**
+ * token结构
+ * */
+export interface token {
+
+  /**
+   * 标签类型
+   * */
+  type: string
+
+  /**
+   * 标签名称，标签token使用
+   * */
+  name?: string
+
+  /**
+   * 文本内容，文本token使用
+   * */
+  content?: string
+
 }
